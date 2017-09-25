850f47602010d4583680278af6e1a83d6371332c
Ian Walter <public@iankwalter.com>
2017-09-25 14:48:52 -0400

    Reverting Windows apm binary change and upgrading packages

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-apm_bin=apm.cmd
-if [[ $(uname) == '' ]]; then
-  apm_bin=apm
-fi
-
 # Install all packages from package file
-$apm_bin install --packages-file packages.txt
+apm install --packages-file packages.txt
 
 # Copy settings file
 cp ./config.cson ~/.atom/config.cson
--- update.sh
+++ update.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-apm_bin=apm.cmd
-if [[ $(uname) == '' ]]; then
-  apm_bin=apm
-fi
-
 # Prompt to upgrade all packages
-$apm_bin upgrade
+apm upgrade
 
 # Save package list to text file
-$apm_bin list --installed --bare > ./packages.txt
+apm list --installed --bare > ./packages.txt
 
 # Copy settings file
 cp ~/.atom/config.cson ./config.cson
